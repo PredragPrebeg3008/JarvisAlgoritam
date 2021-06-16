3833cebad8e848632a7d3ef569fe4a0ec6d02444
PredragPrebeg3008 <2018.10.15@online.osmagimnazija.edu.rs>
2021-06-16 20:49:28 +0200

    kreiranje metode "orijentracija"
    kreiranje metode "orijentracija"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,17 @@ public class Main {
                 if (next[i] != -1)
                     System.out.println("(" + points[i].x + ", " + points[i].y + ")");
         }
-      
+
+        private boolean orijentacija(Point p, Point q, Point r) {
+            int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+
+            if (val >= 0)
+                return false;
+            return true;
+        }
+
+
+
     }
 
     static class Point {
