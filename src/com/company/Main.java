1facbf87da48d1c8f3a4aef375803fbc11e9e9ab
PredragPrebeg3008 <2018.10.15@online.osmagimnazija.edu.rs>
2021-06-16 20:47:11 +0200

    Dodavanje point class
    Dodajemo point klasu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,12 @@ package com.company;
 
 public class Main {
 
+
+    static class Point {
+        int x;
+        int y;
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
