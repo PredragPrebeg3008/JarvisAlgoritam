700d11d8e7a3ffcf5582f530117518bf8b8907df
PredragPrebeg3008 <2018.10.15@online.osmagimnazija.edu.rs>
2021-06-16 20:48:50 +0200

    Kreiranje jarvis klase i metode za ispis
    Kreiranje jarvis klase i metode za ispis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,16 @@ package com.company;
 
 public class Main {
 
+    public static class Jarvis {
+
+        public void ispisi(Point[] points, int[] next) {
+            System.out.println("\nTacke : ");
+            for (int i = 0; i < next.length; i++)
+                if (next[i] != -1)
+                    System.out.println("(" + points[i].x + ", " + points[i].y + ")");
+        }
+      
+    }
 
     static class Point {
         int x;
